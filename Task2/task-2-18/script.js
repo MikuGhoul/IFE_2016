187e100386fbeae88ecdf3e1c21bad5989b04f2b
MikuGhoul <mikughoull@gmail.com>
2017-05-27 20:34:14 +0800

    no abstract?

--- Task2/task-2-18/script.js
+++ Task2/task-2-18/script.js
@@ -1,26 +1,49 @@
-function getValue(){
+
+function getNewDiv(){
     var number=document.getElementById("number");
-    return number.value;
-}
-function produceDiv(value){
     var newDiv=document.createElement("div");
-    newDiv.innerText=value;
+    newDiv.innerText=number.value;
     return newDiv;
 }
+function displayDeleteNum(deletDiv){
+    var displayDiv=document.getElementById("delet");
+    displayDiv.innerText=deletDiv.innerText;
+    var displayDelete=document.getElementById("displayDelet");
+    displayDelete.style.display="block";
+}
 function leftInput(){
-    var value=getValue();
-    var newDiv=produceDiv(value);
+    var newDiv=getNewDiv();
+    var queue=document.getElementById("queue");
+    var firstDiv=queue.getElementsByTagName("div")[0];
+    if (firstDiv){
+        queue.insertBefore(newDiv,firstDiv);
+    } else {
+        rightInput();
+    }
+}
+function rightInput(){
+    var newDiv=getNewDiv();
     var queue=document.getElementById("queue");
     queue.appendChild(newDiv);
 }
-function rightInput(){
-    var value=getValue();
-}
+
 function leftOutput(){
-    var value=getValue();
+    var queue=document.getElementById("queue");
+    var firstDiv=queue.getElementsByTagName("div")[0];
+    if (firstDiv){
+        
+        displayDeleteNum(firstDiv);
+        queue.removeChild(firstDiv);
+    }
 }
 function rightOutput(){
-    var value=getValue();
+    var queue=document.getElementById("queue");
+    var divList=queue.getElementsByTagName("div");
+    if (divList.length){
+        var lastDiv=divList[divList.length-1];
+        displayDeleteNum(lastDiv);
+        queue.removeChild(lastDiv);
+    }
 }
 function init(){
     var handleBox=document.getElementById("handle");
@@ -34,6 +57,13 @@ function init(){
                 leftOutput();break;
             case "右输出":
                 rightOutput();break;
+        }
+    });
+    var queueBox=document.getElementById("queue");
+    queueBox.addEventListener("click",function (e){
+        if (e.target.getAttribute("id")!="queue"){
+            displayDeleteNum(e.target);
+            queueBox.removeChild(e.target);
         }
     })
 }
